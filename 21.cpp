7dc5b2973a5c8b3fd5f47a02a1776bb19a268499
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-06-01 16:35:06 +0530

    Problem with 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -9,9 +9,25 @@ void timer(long millis, string name){
 }
 
 int main(){
-    thread t1(timer, 1000, "tim1");
-    thread t2(timer, 2000, "tim2");
-    thread t3(timer, 3000, "tim3");
+    long dur1;
+    long dur2;
+    long dur3;
+    cout << "How many minutes(Timer1)? ";
+    cin << dur1;
+    cout << endl;
+
+    cout << "How many minutes(Timer2)? ";
+    cin << dur2;
+    cout << endl;
+
+    cout << "How many minutes(Timer3)? ";
+    cin << dur3;
+    cout << endl;
+
+
+    thread t1(timer, dur1*60000, "tim1");
+    thread t2(timer, dur2*60000, "tim2");
+    thread t3(timer, dur3*60000, "tim3");
     t1.join();
     t2.join();
     t3.join();
